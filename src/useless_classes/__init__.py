7725929aff4d2ea4ca960f767888116737ae2434
pswsm <53122461+pswsm@users.noreply.github.com>
2025-12-12 14:37:28 +0100

    feat: skeleton: return correct types on callables
    Skeleton class now returns correct types on callables

--- src/useless_classes/__init__.py
+++ src/useless_classes/__init__.py
@@ -1,35 +1,7 @@
-from typing import Any, Self, Type
+from typing import Any, Self, Type, get_type_hints
 
 
 FORBIDDEN_METHODS = {"class", "dict", "weakref", "dir"}
-
-
-def empty():
-    ...
-
-
-class Skeleton:
-    def __init__(self, proto: Type) -> None:
-        """Class to use in place of others. Copies the methods and attributes of proto.
-
-        Args:
-            proto(Type): Class to copy from.
-        """
-        self._proto = proto
-        self._discover()
-
-    def _discover(self):
-        for attr in {a for a in dir(self._proto) if a.strip("_") not in FORBIDDEN_METHODS}:
-            if isinstance(attr, type(empty)):
-                setattr(self, attr, lambda *args, **kwargs: NullClass())
-                continue
-            setattr(self, attr, NullClass())
-
-    def __bool__(self) -> bool:
-        try:
-            return self._proto().__bool__()
-        except Exception as _:
-            return False
 
 
 class NullClass:
@@ -56,3 +28,60 @@ class NullClass:
         if isinstance(value, NullClass):
             return True
         return False
+
+
+class Skeleton:
+    def __init__(self, proto: Type, /) -> None:
+        """Class to use in place of others. Copies the methods and attributes of proto.
+
+        Args:
+            proto(Type): Class to copy from.
+        """
+        if isinstance(proto, Skeleton):
+            raise ValueError("Cannot create Skeleton of a Skeleton")
+        if isinstance(proto, NullClass):
+            raise ValueError("Cannot create Skeleton of a NullClass")
+        self._proto = proto
+        self._discover()
+
+    def _dummy_return(self, return_type: type, /):
+        try:
+            return RETURNS[return_type]
+        except KeyError as _:
+            return NullClass()
+
+    def _discover(self):
+        for attr in {
+            getattr(self._proto, a)
+            for a in dir(self._proto)
+            if a.strip("_") not in FORBIDDEN_METHODS
+        }:
+            if not callable(attr):
+                setattr(self, attr, NullClass())
+            try:
+                return_type: type = get_type_hints(attr)["return"]
+            except KeyError as _ke:
+                return_type = NullClass
+            setattr(
+                self,
+                attr.__name__,
+                lambda *args, **kwargs: self._dummy_return(return_type),
+            )
+            continue
+
+    def __bool__(self) -> bool:
+        try:
+            return self._proto().__bool__()
+        except Exception as _:
+            return False
+
+
+RETURNS: dict[type, Any] = {
+    str: "",
+    int: -1,
+    float: -0.1,
+    list: [],
+    dict: {"skeleton": True},
+    set: {"skeleton"},
+    NullClass: NullClass(),
+}
