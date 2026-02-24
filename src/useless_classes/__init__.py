cb5021184f681ab0b1c254299ed511df66fb27be
pswsm <53122461+pswsm@users.noreply.github.com>
2026-02-24 16:48:14 +0100

    refactor: rename to NullObject

--- src/useless_classes/__init__.py
+++ src/useless_classes/__init__.py
@@ -1,10 +1,13 @@
-from typing import Any, Self, Type, get_type_hints
-
+from typing import Any, Self, Type, final, get_type_hints
 
 FORBIDDEN_METHODS = {"class", "dict", "weakref", "dir"}
 
 
-class NullClass:
+@final
+class NullObject:
+    def __init__(self):
+        pass
+
     def __call__(self, *_args: Any, **_kwds: Any) -> Self:
         return self
 
@@ -25,7 +28,7 @@ class NullClass:
         return "NullClass"
 
     def __eq__(self, value: object, /) -> bool:
-        if isinstance(value, NullClass):
+        if isinstance(value, NullObject):
             return True
         return False
 
@@ -34,12 +37,14 @@ class Skeleton:
     def __init__(self, proto: Type, /) -> None:
         """Class to use in place of others. Copies the methods and attributes of proto.
 
+        WARNING: Unstable
+
         Args:
             proto(Type): Class to copy from.
         """
         if isinstance(proto, Skeleton):
             raise ValueError("Cannot create Skeleton of a Skeleton")
-        if isinstance(proto, NullClass):
+        if isinstance(proto, NullObject):
             raise ValueError("Cannot create Skeleton of a NullClass")
         self._proto = proto
         self._discover()
@@ -48,7 +53,7 @@ class Skeleton:
         try:
             return RETURNS[return_type]
         except KeyError as _:
-            return NullClass()
+            return NullObject()
 
     def _discover(self):
         for attr in {
@@ -57,11 +62,11 @@ class Skeleton:
             if a.strip("_") not in FORBIDDEN_METHODS
         }:
             if not callable(attr):
-                setattr(self, attr, NullClass())
+                setattr(self, attr, NullObject())
             try:
                 return_type: type = get_type_hints(attr)["return"]
             except KeyError as _ke:
-                return_type = NullClass
+                return_type = NullObject
             setattr(
                 self,
                 attr.__name__,
@@ -83,5 +88,5 @@ RETURNS: dict[type, Any] = {
     list: [],
     dict: {"skeleton": True},
     set: {"skeleton"},
-    NullClass: NullClass(),
+    NullObject: NullObject(),
 }
